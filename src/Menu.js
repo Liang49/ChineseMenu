d77762b05e4709ae52b15b286a74e5b57f5c8933
Liang49 <liangzhang2014@gmail.com>
2019-05-28 13:07:05 -0400

    button
    add onclicks

--- src/Menu.js
+++ src/Menu.js
@@ -5,22 +5,26 @@ class Menu extends React.Component {
     return (
       <div>
         <h1>Fried Rice</h1>
-        <button onClick={this.handleClick}>Chicken Fried Rice</button>
-        <button>Pork Fried Rice</button>
-        <button>Golden Fried Rice</button>
-        <button>House Special Fried Rice</button>
-        <button>Beef Fried House</button>
+        <button onClick={this.props.handleClick}>Chicken Fried Rice</button>
+        <button onClick={this.props.handleClick}>Pork Fried Rice</button>
+        <button onClick={this.props.handleClick}>Golden Fried Rice</button>
+        <button onClick={this.props.handleClick}>
+          House Special Fried Rice
+        </button>
+        <button onClick={this.props.handleClick}>Beef Fried House</button>
         <h1>Lo Mein</h1>
-        <button>Chicken Lo Mein</button>
-        <button>Pork Lo Mein</button>
-        <button>Beef Lo Mein</button>
-        <button>SeaFood Lo Mein</button>
+        <button onClick={this.props.handleClick}>Chicken Lo Mein</button>
+        <button onClick={this.props.handleClick}>Pork Lo Mein</button>
+        <button onClick={this.props.handleClick}>Beef Lo Mein</button>
+        <button onClick={this.props.handleClick}>SeaFood Lo Mein</button>
         <h1>Lunch Special</h1>
         <button onClick={this.props.handleClick}>Chicken and Broccoli</button>
-        <button>Salt and Pepper Pork Chop</button>
-        <button>Fried Chicken Wings</button>
-        <button>Singapore Mei Fun</button>
-        <button>Curry Chicken</button>
+        <button onClick={this.props.handleClick}>
+          Salt and Pepper Pork Chop
+        </button>
+        <button onClick={this.props.handleClick}>Fried Chicken Wings</button>
+        <button onClick={this.props.handleClick}>Singapore Mei Fun</button>
+        <button onClick={this.props.handleClick}>Curry Chicken</button>
       </div>
     );
   }
